587e1a58226790418493ef86139c98e75571f45c
DavidK <dhkim@andrews.edu>
2025-08-26 01:09:44 -0400

    cosmetic

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -30,7 +30,7 @@ const Navigation = () => {
                 <Calendar className="w-5 h-5 text-primary-foreground" />
               </div>
               <span className="text-xl font-bold bg-gradient-primary bg-clip-text text-transparent">
-                EventHub
+                Korean Adventist Events
               </span>
             </Link>
           </div>
